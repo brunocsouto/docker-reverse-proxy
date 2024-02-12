53d7d3b40c19f92f4fcc36e6462d9b5b0e9fa378
Bruno Souto <bruno@souto.dev>
2024-02-11 22:14:32 -0300

    feat: initial structure

---
+++ node/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+const config = {
+  host: 'db',
+  database: 'nodedb',
+  user: 'root',
+  password: 'root',
+};
+
+app.get('/:name', (req, res) => {
+  const connection = mysql.createConnection(config);
+  const name = req.params.name;
+
+  if (name) {
+    const insertQuery = `INSERT INTO people(name) values('${name}')`;
+    connection.query(insertQuery);
+  }
+
+  const selectNamesQuery = `SELECT * FROM people`;
+  connection.query(selectNamesQuery, (_, result) => {
+    const selectResult = JSON.parse(JSON.stringify(result)) || [];
+
+    let namesList = '';
+    selectResult?.forEach((row) => {
+      namesList += `<li>${row.name}</li>`;
+    });
+
+    res
+      .status(200)
+      .send(
+        `<h1>Full Cycle Rocks</h1><p>Try type a name in the URL: <strong>localhost/[name]</strong></p><ul>${namesList}</ul>`
+      );
+    connection.end();
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Node running on port ${PORT}`);
+});
